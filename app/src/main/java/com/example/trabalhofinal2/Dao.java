2656872277fd754cff3163a2fd475e8c9b6ebcf5
weslei <wesleipatrick@gmail.com>
2019-05-07 07:33:15 -0300

    Init

---
+++ app/src/main/java/com/example/trabalhofinal2/Dao.java
@@ -0,0 +1,71 @@
+package com.example.trabalhofinal2;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Dao extends SQLiteOpenHelper {
+
+    private SQLiteDatabase db = getReadableDatabase();
+
+    public Dao(Context context) {
+        super(context, "pontos2", null, 2);
+    }
+
+    void addPontoTuristico(Ponto pontoTuristico) {
+        SQLiteDatabase db = getReadableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put("nome", pontoTuristico.getNome());
+        values.put("latitude", pontoTuristico.getLatitude());
+        values.put("longitude", pontoTuristico.getLongitude());
+        values.put("endereco", pontoTuristico.getEndereco());
+        values.put("imagem", pontoTuristico.getImage());
+
+        db.insert("pontos2", null, values);
+    }
+
+    List<Ponto> listarPontos() {
+        String sql = "SELECT * FROM pontos2";
+        Cursor c = db.rawQuery(sql, null);
+        List<Ponto> pontos = new ArrayList<>();
+
+        int cnt = 0;
+
+        while (c.moveToNext()) {
+            Ponto pontoTuristico = new Ponto();
+            pontoTuristico.setNome(c.getString(c.getColumnIndex("nome")));
+            pontoTuristico.setLatitude(c.getString(c.getColumnIndex("latitude")));
+            pontoTuristico.setLongitude(c.getString(c.getColumnIndex("longitude")));
+            pontoTuristico.setEndereco(c.getString(c.getColumnIndex("endereco")));
+            pontoTuristico.setImage(c.getBlob(c.getColumnIndex("imagem")));
+            cnt++;
+//            System.out.println("Ponto: "+cnt);
+            Log.e("Ponto", "Ponto: "+cnt);
+            pontos.add(pontoTuristico);
+        }
+        c.close();
+
+        return pontos;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqlDb) {
+        String sqlCreate = "CREATE TABLE IF NOT EXISTS pontos2(nome VARCHAR, latitude VARCHAR, longitude VARCHAR, endereco VARCHAR, imagem BLOB);";
+        if (db == null) {
+            db = sqlDb;
+        }
+        db.execSQL(sqlCreate);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
---
+++ app/src/main/java/com/example/trabalhofinal2/MapsActivity.java
@@ -0,0 +1,75 @@
+package com.example.trabalhofinal2;
+
+import android.database.Cursor;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private Dao dao;
+
+    private MarkerOptions options = new MarkerOptions();
+    private ArrayList<LatLng> latlngs = new ArrayList<>();
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+
+        mapFragment.getMapAsync(this);
+
+        dao = new Dao(MapsActivity.this);
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        LatLng ponto = new LatLng(-25.73560254, -53.05798153);
+        List<Ponto> pontos = dao.listarPontos();
+
+        for( int i = 0; i < pontos.size(); i++ ){
+
+            Log.i("Carregar Pontos", "Ponto: "+i);
+            Log.i("Carregar Pontos", ""+pontos.get(i).getNome() );
+            Log.i("Carregar Pontos", "Latitude: "+pontos.get(i).getLatitude() );
+            Log.i("Carregar Pontos", "Longitude: "+pontos.get(i).getLongitude() );
+
+            Log.i("Carregar Pontos", "-----");
+
+            ponto = new LatLng(Double.parseDouble( pontos.get(i).getLatitude() ), Double.parseDouble( pontos.get(i).getLongitude() ) );
+
+            mMap.addMarker(new MarkerOptions().position(
+                    ponto
+            ).title( pontos.get(i).getNome() ));
+
+        }
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(
+                ponto
+        ));
+
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                ponto, 7));
+
+    }
+
+
+
+}
